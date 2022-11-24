62bb5c13893ccf2a481ba332a8f45d2a6e3fb14f
esther <esther.githinji@student.moringaschool.com>
2022-11-24 23:46:17 +0300

    completed Assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+//  const divide = function(a,b){
+//     return (a/b)
+//  }
+//  divide (2000, 100)
+ 
+  
+//     it("has an arrow function called add", () => {
+//       expect(add).to.exist
+//     })
+  
+//     it("add arrow function takes two parameters and adds them together", () => {
+//       let a = 3
+//       let b = 4
+//       expect(add(a,b)).to.equal(a+b)
+//     })
+//   })
+  
+
+const divide = (a, b) => (2000/100)
+
+const square = x => (2**2)
+
+// function add(a, b){
+//     let a= 3
+//     let b= 4
+//     return (a+b)
+// }
+// add()
+
+const add = (a=3, b=4) => (a+b)
+    
